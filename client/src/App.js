fe8ff9132a93ac5e4e34d65f481ec69196b2f203
KY <kaliyamoorthy.2k@gmail.com>
2024-08-18 20:22:17 +0530

    feat: video, audio and screen share within group call

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { useNavigate, useParams } from "react-router";
+
 import { Device } from "mediasoup-client";
 import io from "socket.io-client";
-import { Button } from "antd";
+
+import { Button, Input } from "antd";
 
 import "./App.css";
 
-const socket = io("https://192.168.220.186:3300");
+import { customAlphabet } from "nanoid";
+const nanoid = customAlphabet("abcdefghijlmnopqrstvwxyz");
+
+const socket = io("https://192.168.246.187:3300");
 const device = new Device();
 
-let params = {
+let videoParams = {
   // https://mediasoup.org/documentation/v3/mediasoup-client/api/#transport-produce
   encodings: [
     {
@@ -34,80 +40,83 @@ let params = {
   },
 };
 
-let rtpCapabilities;
+let audioParams = {}
+
 let producerTransport;
-let consumerTransport;
-let producer;
-let consumer;
+let consumers = [];
+let videoProducer;
+let producerIds = [];
+let remoteProducerIds = []
+let audioProducer;
 
 function App() {
   const videoClientRef = useRef(null);
-  const videoRemoteRef = useRef(null);
+  const containerRef = useRef(null);
 
-  /** node:00
+  const navigate = useNavigate();
+  const { roomId } = useParams();
+  const [hasJoined, setHasJoined] = useState(false);
+  const [userName, setUserName] = useState("Unknown")
+
+  /**
    * get user media
    **/
-
   const getUserMedia = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          width: 800,
-          height: 450,
-          facingMode: "environment",
-        },
-        // audio: true,
+    let screenStream;
+    let videoStream;
+    let audioStream;
+
+    try{
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true
       });
 
-      videoClientRef.current.srcObject = stream;
+      screenStream = stream.getVideoTracks()[0]
+    }catch(error){
+      console.log(error)
+      alert(error);
+    }
 
-      console.log("00: get media streams", stream.getTracks());
+    try{
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      audioStream = stream.getAudioTracks()[0]
+      videoStream = stream.getVideoTracks()[0]
+    }catch(error){
+      console.log(error)
+      alert(error);
+    }
 
-      params = {
-        track: stream.getTracks()[0],
-        ...params,
+    try {
+      console.log("00: get media streams", screenStream, videoStream, audioStream);
+      videoStream = screenStream ? screenStream : videoStream
+      videoParams = {
+        track: videoStream,
+        ...videoParams,
       };
+
+      audioParams = {
+        track: audioStream
+      }
+
+      videoClientRef.current.srcObject = new MediaStream([videoStream]);
+
     } catch (error) {
       console.log("Error/get media streams:", error);
       alert(error);
     }
   };
 
-  /** node : 01
-   * get RTP Capabilities
-   **/
-  const getRtpCapabilities = () => {
-    socket.emit("GET_RTP_CAPABILITIES", (params) => {
-      console.log("01: get rtp capabilities.", params.rtpCapabilities);
-      rtpCapabilities = params.rtpCapabilities;
-    });
-  };
-
   /**
-   * node:02
-   * create device
-   **/
-  const createDevice = async () => {
-    try {
-      await device.load({
-        routerRtpCapabilities: rtpCapabilities,
-      });
-      console.log("02: create device.", device);
-      // createSendTransport();
-    } catch (error) {
-      console.log("Error/create device:", error);
-    }
-  };
-
-  /**
-   * node:03
-   * create send transport
+   * create producer transport and start produce
    */
-  const createSendTransport = async () => {
-    // get transport parameters from server
-    socket.emit("CREATE_WEBRTC_TRANSPORT", { sender: true }, async (params) => {
+  const createProducerTransport = async () => {
+    // create send transport
+    socket.emit("WEBRTC_TRANSPORT", { consumer: false }, async (params) => {
       if (params.error) {
-        console.log("Error/create send transport:", params.error);
+        console.log("Error/create producer transport:", params);
         return;
       }
 
@@ -121,7 +130,7 @@ function App() {
           try {
             console.log("05: producer transport on connect:", dtlsParameters);
             // connect send transport on server
-            await socket.emit("TRANSPORT_CONNECT", dtlsParameters);
+            await socket.emit("CONNECT_PRODUCER", dtlsParameters);
 
             // must be invoked to tell server dtls parameter transferred
             callback();
@@ -137,189 +146,236 @@ function App() {
 
         try {
           const { kind, rtpParameters } = params;
-          await socket.emit(
-            "TRANSPORT_PRODUCE",
-            { kind, rtpParameters },
-            (id) => {
-              callback({ id });
-              // [for now creating immediate consumer]
-              // createReceiverTransport();
-            }
-          );
+          await socket.emit("START_PRODUCE", { kind, rtpParameters }, (id) => {
+            
+            callback({ id });
+
+            socket.emit(`SIGNAL/NEW_MEMBER/${roomId}`, id);
+            producerIds.push(id);
+          });
         } catch (error) {
           console.log("Error/producer transport on produce:", error);
           errback(error);
         }
       });
 
-      // connect send transport
-      // connectSendTransport();
+      // connect producer transport and start produce
+      console.log("04: connect producer transport.", videoParams);
+
+      audioProducer = await producerTransport.produce(audioParams);
+      videoProducer = await producerTransport.produce(videoParams);
+
+      videoProducer.on("trackended", () => {
+        console.log("video ended.");
+      });
+
+      videoProducer.on("transportclose", () => {
+        console.log("transport closed.");
+      });
+
+
+      audioProducer.on("trackended", () => {
+        console.log("audio ended.");
+      });
+
+      audioProducer.on("transportclose", () => {
+        console.log("transport closed.");
+      });
+
     });
   };
 
   /**
-   * node:04
-   * connect send transport
-   * */
-  const connectSendTransport = async () => {
-    
-    console.log("04: connect send transport.", params);
-    producer = await producerTransport.produce(params);
-    
-    producer.on("trackended", () => {
-      console.log("video ended.");
-    });
-
-    producer.on("transportclose", () => {
-      console.log("transport closed.");
-    });
-  };
-
-  /**
-   * node: 07
-   * create receiver transport
+   * create consumer transport and start consume
    */
-  const createReceiverTransport = () => {
-    socket.emit(
-      "CREATE_WEBRTC_TRANSPORT",
-      { sender: false },
-      async (params) => {
-        consumerTransport = device.createRecvTransport(params);
-        console.log("03: create receiver transport.", consumerTransport);
+  const createConsumerTransport = (remoteProducerId) => {
+    socket.emit("WEBRTC_TRANSPORT", { consumer: true }, async (params) => {
+      if (params.error) {
+        console.log("Error/create consumer transport:", params);
+        return;
+      }
 
-        // consumer transport on connect
-        consumerTransport.on(
-          "connect",
-          async ({ dtlsParameters }, callback, errback) => {
-            try {
-              console.log("05: on consumer connect.", dtlsParameters);
-              await socket.emit("CONNECT_CONSUMER_TRANSPORT", dtlsParameters);
-              callback();
-            } catch (error) {
-              errback(errback);
+      let consumerTransport = device.createRecvTransport(params);
+      console.log("03: create receiver transport.", consumerTransport);
+
+      // consumer transport on connect
+      consumerTransport.on(
+        "connect",
+        async ({ dtlsParameters }, callback, errback) => {
+          try {
+            console.log("05: on consumer connect.", dtlsParameters);
+            await socket.emit("CONNECT_CONSUMER", {
+              consumerId: params.id,
+              dtlsParameters,
+            });
+            callback();
+          } catch (error) {
+            errback(errback);
+          }
+        }
+      );
+
+      // start consume
+      socket.emit(
+        "START_CONSUME",
+        {
+          rtpCapabilities: device.rtpCapabilities,
+          remoteProducerId: remoteProducerId,
+          consumerId: params.id,
+        },
+        async (params) => {
+          let consumer = await consumerTransport.consume({
+            producerId: params.producerId,
+            id: params.consumerId,
+            rtpParameters: params.rtpParameters,
+            kind: params.kind,
+          });
+
+          const { track } = consumer;
+
+          // add consumer and transport
+          consumers.push({
+            remoteProducerId: remoteProducerId,
+            consumerId: params.consumerId,
+            consumer,
+            transport: consumerTransport,
+          });
+
+          if(params.kind === "video"){
+            const _video = document.querySelector(`video#${btoa(params.consumerId)}`);
+            if (!_video) {
+              const video = document.createElement("video");
+              video.autoplay = true;
+              video.id = btoa(params.consumerId);
+              video.srcObject = new MediaStream([track]);
+              containerRef.current.appendChild(video);
+            }
+
+          }
+
+          if(params.kind === "audio"){
+            const _audio = document.querySelector(`audio#${btoa(params.consumerId)}`);
+            if (!_audio) {
+              const audio = document.createElement("audio");
+              audio.autoplay = true;
+              audio.id = btoa(params.consumerId);
+              audio.srcObject = new MediaStream([track]);
+              containerRef.current.appendChild(audio);
             }
           }
-        );
-
-        // connectReceiverTransport();
-      }
-    );
+          
+          // resume stream
+          socket.emit("RESUME", { consumerId: params.consumerId });
+        }
+      );
+    });
   };
 
   /**
-   * node:08
-   * connect receiver transport
-   */
-  const connectReceiverTransport = async () => {
-    socket.emit(
-      "TRANSPORT_CONSUME",
-      { rtpCapabilities: device.rtpCapabilities },
-      async (params) => {
+   * create room and join
+  */
+  const createRoomId = async () => {
+    const start = nanoid(3);
+    const middle = nanoid(4);
+    const end = nanoid(3);
+    const _roomId = `${start}-${middle}-${end}`;
 
-        consumer = await consumerTransport.consume({
-          producerId: params.producerId,
-          id: params.consumerId,
-          rtpParameters: params.rtpParameters,
-          kind: params.kind,
+    // if room does not exist create and navigate to new room id
+    if (!roomId) {
+      navigate(`/room/${_roomId}`);
+      return;
+    }
+
+    if (hasJoined) return;
+
+    // attach new member trigger socket
+    socket.on(`NEW_MEMBER/${roomId}`, (_producerIds) => {
+      console.log("new-members", _producerIds);
+      _producerIds.forEach(({ id }) => {
+        if (!producerIds.includes(id) && !remoteProducerIds.includes(id)) {
+          remoteProducerIds.push(id);
+          createConsumerTransport(id);
+        }
+      });
+    });
+
+    // attach member left socket
+    socket.on("MEMBER_LEFT", ({ remoteProducerId }) => {
+      const consumerData = consumers.find(({ remoteProducerId: id }) => id === remoteProducerId);
+      
+      if (consumerData) {
+        const { consumer, transport } = consumerData;
+        consumer.close();
+        transport.close();
+
+        // clean up
+        consumers = consumers.filter(({ id }) => id !== remoteProducerId);
+        remoteProducerIds = remoteProducerIds.filter((id) => id !== remoteProducerId);
+
+        // remove elements
+        const video = document.querySelector(`video#${btoa(consumer.id)}`);
+        const audio = document.querySelector(`audio#${btoa(consumer.id)}`);
+        if (video) {
+          containerRef.current.removeChild(video);
+        }
+        if (audio) {
+          containerRef.current.removeChild(audio);
+        }
+
+      }
+    });
+
+    try {
+      await getUserMedia();
+      socket.emit("JOIN_ROOM", { roomId, user : { name: userName } }, async (params) => {
+        let rtpCapabilities = params.rtpCapabilities;
+        await device.load({
+          routerRtpCapabilities: rtpCapabilities,
         });
 
-        console.log("06: on consume.", consumer);
+        createProducerTransport();
+        setHasJoined(true);
 
-        const { track } = consumer;
-
-        videoRemoteRef.current.srcObject = new MediaStream([track]);
-        socket.emit("RESUME");
-      }
-    );
+        console.log("rtp capabilities", rtpCapabilities);
+      });
+    } catch (error) {
+      console.log("error/createRoom");
+    }
   };
-
-  /**
-   * node: start
-   * mediasoup
-   **/
-  // useEffect(() => {
-  //   (async () => {
-  //     console.log("00: get user media.");
-  //     await getUserMedia();
-
-  //     /**
-  //      * node:01
-  //      * get RTP capabilities from server and create device.
-  //      **/
-  //     socket.emit("GET_RTP_CAPABILITIES", (params) => {
-  //       console.log("01: get rtp capabilities.", params.rtpCapabilities);
-  //       rtpCapabilities = params.rtpCapabilities;
-  //       createDevice();
-  //     });
-  //   })();
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   return (
     <Container>
-      <section className="videos-container">
-        <section>
-          <video ref={videoClientRef} autoPlay></video>
-          <section className="btn-container">
-            {/* <Button type="default" onClick={createSendTransport} size="large">
-              Produce
-            </Button> */}
-          </section>
-        </section>
-        <section>
-          <video ref={videoRemoteRef} autoPlay></video>
-          <section className="btn-container">
-            {/* <Button type="default" onClick={createReceiverTransport} size="large">
-              Consume
-            </Button> */}
-          </section>
-        </section>
+      <section className="videos-container" ref={containerRef}>
+        <video ref={videoClientRef} autoPlay></video>
       </section>
+
+      <hr />
 
       <section className="execute-buttons-container">
         <section>
-          <Button type="default" onClick={getUserMedia} size="large">
-            Get Media streams
-          </Button>
-          <Button type="default" onClick={getRtpCapabilities} size="large">
-            Get RTP Capabilities
-          </Button>
-          <Button type="default" onClick={createDevice} size="large">
-            Create Device
-          </Button>
-          <Button type="default" onClick={createSendTransport} size="large">
-            Create Send Transport
-          </Button>
-          <Button type="default" onClick={connectSendTransport} size="large">
-            Connect Send Transport
-          </Button>
+          <Input placeholder="Enter your name"  onChange={(e)=>{
+            setUserName(e.target.value )
+          }}/>
         </section>
-
         <section>
-          <Button type="default" onClick={getRtpCapabilities} size="large">
-            Get RTP Capabilities
-          </Button>
-          <Button type="default" onClick={createDevice} size="large">
-            Create Device
-          </Button>
-          <Button type="default" onClick={createReceiverTransport} size="large">
-            Create Receiver Transport
-          </Button>
           <Button
             type="default"
-            onClick={connectReceiverTransport}
-            size="large"
+            onClick={createRoomId}
+            disabled={hasJoined}
+            size="medium"
           >
-            Connect Receiver Transport
+            {roomId ? (hasJoined ? "Joined" : "Join Room") : "Create Room"}
           </Button>
         </section>
       </section>
+      <hr />
     </Container>
   );
 }
 
 const Container = styled.div`
+  padding: 1rem;
+
   .videos-container {
     display: grid;
     grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -327,6 +383,7 @@ const Container = styled.div`
     video {
       width: 100%;
       aspect-ratio: 16/9;
+      border-radius: 8px;
     }
   }
 
@@ -340,7 +397,6 @@ const Container = styled.div`
   }
 
   .execute-buttons-container {
-    padding: 1rem;
     display: grid;
     grid-template-columns: repeat(2, minmax(0, 1fr));
     gap: 1rem;
@@ -350,6 +406,12 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
+  }
+
+  hr {
+    border: 0;
+    border-bottom: 1px dashed #d5d5d5;
+    margin: 1rem;
   }
 `;
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "room/:roomId",
+    element: <App />,
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
